1ef49b16668e1c44460da29a73ebc560a567ea2d
Keshav Sharma <keshavs642@gmail.com>
2020-12-02 15:44:16 +0530

    Using slots

--- userCard.js
+++ userCard.js
@@ -1,3 +1,44 @@
+//Create template
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        .user-card {
+            font-family: 'Arial', sans-serif;
+            background: #f4f4f4;
+            width: 500px;
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            grid-gap: 10px;
+            margin-bottom: 15px;
+            border-bottom: darkorchid 5px solid;
+        }
+
+        .user-card img {
+            width: 100%;
+        }
+
+        .user-card button {
+            cursor: pointer;
+            background: darkorchid;
+            color: #fff;
+            border: 0;
+            border-radius: 5px;
+            padding: 5px 10px;
+        }
+    </style>
+    <div class="user-card">
+        <img />    
+        <div>
+            <h3></h3>
+            <div class="info">
+                <p><slot name="email" /></p>
+                <p><slot name="phone" /></p>
+            </div>
+            <button id="toggle-info">Hide Info</button>
+        </div>
+     </div>
+`;
+
 //A custom element
 class UserCard extends HTMLElement
 {
@@ -5,7 +46,12 @@ class UserCard extends HTMLElement
     constructor()
     {
         super(); //Calls the cosntructor of HTMLElement
-        this.innerHTML = `<h3>${this.getAttribute('name')}</h3>`;
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+        this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
+        
     }
 }
 
