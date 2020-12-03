5e6db5630c5966e3b29023640a4d1f556fea71a5
Keshav Sharma <keshavs642@gmail.com>
2020-12-03 16:28:37 +0530

    How to link a css file in the template

--- userCard.js
+++ userCard.js
@@ -1,31 +1,9 @@
 //Create template
 const template = document.createElement('template');
 template.innerHTML = `
-    <style>
-        .user-card {
-            font-family: 'Arial', sans-serif;
-            background: #f4f4f4;
-            width: 500px;
-            display: grid;
-            grid-template-columns: 1fr 2fr;
-            grid-gap: 10px;
-            margin-bottom: 15px;
-            border-bottom: darkorchid 5px solid;
-        }
-
-        .user-card img {
-            width: 100%;
-        }
-
-        .user-card button {
-            cursor: pointer;
-            background: darkorchid;
-            color: #fff;
-            border: 0;
-            border-radius: 5px;
-            padding: 5px 10px;
-        }
-    </style>
+    
+    <link rel="stylesheet" href="yoyo.css">
+   
     <div class="user-card">
         <img />    
         <div>
@@ -47,16 +25,22 @@ class UserCard extends HTMLElement
     {
         super(); //Calls the cosntructor of HTMLElement
         this.showInfo = true;
-
+        console.log('constructor');
+        console.log('1');
         this.attachShadow({ mode: 'open' });
+        console.log('2');
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+        console.log('3');
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+        console.log('4');
         this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
+        console.log('5');
         
     }
 
     toggleInfo()
     {
+        console.log('toggle');
         this.showInfo = !this.showInfo;
         const info = this.shadowRoot.querySelector('.info');
         const toggleBtn = this.shadowRoot.querySelector('#toggle-info');
@@ -76,12 +60,14 @@ class UserCard extends HTMLElement
     //called when element is inserted into the DOM
     connectedCallback()
     {
+        console.log('connectedCallback');
         this.shadowRoot.querySelector('#toggle-info').addEventListener('click', () => this.toggleInfo());
     }
 
     //Called when element is removed from the DOM
     disconnectedCallback()
     {
+        console.log('disconnectedCallback');
         this.shadowRoot.querySelector('#toggle-info').removeEventListener();
     }
 
@@ -89,4 +75,7 @@ class UserCard extends HTMLElement
 }
 
 ///Define custom element
+//The function prototype is : CustomElementRegistry.define()
+//window.customElements returns a reference to the CustomElementRegistry object
+console.log('last line');
 window.customElements.define('user-card', UserCard); //Take in 2 params - name of the tag, and the class we want to connect it to. 
